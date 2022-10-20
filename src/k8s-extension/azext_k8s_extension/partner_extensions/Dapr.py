22ae5fd608c11c2b50a69c573c90c6d392cb8108
Shubham Sharma <shubhash@microsoft.com>
2022-10-20 10:42:49 +0530

    Add logging
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- src/k8s-extension/azext_k8s_extension/partner_extensions/Dapr.py
+++ src/k8s-extension/azext_k8s_extension/partner_extensions/Dapr.py
@@ -6,12 +6,19 @@
 # pylint: disable=unused-argument
 # pylint: disable=line-too-long
 # pylint: disable=too-many-locals
+# pylint: disable=too-many-instance-attributes
+
+import copy
+from knack.log import get_logger
 
 from .DefaultExtension import DefaultExtension
 
 from ..vendored_sdks.models import (
     Extension,
+    PatchExtension,
 )
+
+logger = get_logger(__name__)
 
 
 class Dapr(DefaultExtension):
@@ -23,7 +30,6 @@ class Dapr(DefaultExtension):
                extension_type, scope, auto_upgrade_minor_version, release_train, version, target_namespace,
                release_namespace, configuration_settings, configuration_protected_settings,
                configuration_settings_file, configuration_protected_settings_file):
-
         """ExtensionType 'Microsoft.Dapr' specific validations & defaults for Create
            Must create and return a valid 'ExtensionInstance' object.
         """
@@ -44,3 +50,31 @@ class Dapr(DefaultExtension):
             location=""
         )
         return extension_instance, name, create_identity
+
+    def Update(self, cmd, resource_group_name: str, cluster_name: str, auto_upgrade_minor_version: bool,
+               release_train: str, version: str, configuration_settings: dict,
+               configuration_protected_settings: dict, original_extension: Extension, yes: bool) \
+            -> PatchExtension:
+
+        input_configuration_settings = copy.deepcopy(configuration_settings)
+
+        # configuration_settings can be None, so we need to set it to an empty dict.
+        if configuration_settings is None:
+            configuration_settings = {}
+
+        # If we are downgrading the extension, then we need to disable the apply-CRDs hook.
+        # This is because CRD updates while downgrading can cause issues.
+        original_version = original_extension.version
+        if original_version and version and version < original_version:
+            configuration_settings['hooks.applyCrds'] = 'false'
+            logger.debug("Downgrade detected. Setting hooks.applyCrds to false.")
+
+        # If no changes were made, return the original dict (empty or None).
+        if len(configuration_settings) == 0:
+            configuration_settings = input_configuration_settings
+
+        return PatchExtension(auto_upgrade_minor_version=auto_upgrade_minor_version,
+                              release_train=release_train,
+                              version=version,
+                              configuration_settings=configuration_settings,
+                              configuration_protected_settings=configuration_protected_settings)
